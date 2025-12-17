0ea0f593328f14d94bec2b66d8c8d5ee66613dea
DAMIANHE <66206811+DAMIANHE@users.noreply.github.com>
2025-12-17 11:38:29 -0300

    Create exampleData.js

---
+++ data/exampleData.js
@@ -0,0 +1,9 @@
+const exampleData = [
+  { variable: "Inflación", supuesto: 60, realidad: 72, peso: 0.18 },
+  { variable: "Morosidad", supuesto: 55, realidad: 68, peso: 0.22 },
+  { variable: "Fondeo", supuesto: 50, realidad: 55, peso: 0.12 },
+  { variable: "Liquidez", supuesto: 62, realidad: 58, peso: 0.14 },
+  { variable: "Capital", supuesto: 70, realidad: 64, peso: 0.15 }
+];
+
+export default exampleData;
